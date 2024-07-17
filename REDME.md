781c9189558c521c6dd543f74e15024769544662
akshaynkotkar <akshaynkotkar@gmail.com>
2024-07-17 19:18:18 +0530

    create,update, delete completed

--- REDME.md
+++ REDME.md
@@ -16,3 +16,27 @@ inside package.json file add type="module"
  npm  i node --save ->every time no need to start the server
  "start":"nodemon ./index.js",
  add inside script
+
+
+ npm i body-parser --save
+
+Read  Details API test
+ ![alt text](<Screenshot 2024-07-17 184238.png>)
+
+Adding Record i.e create nw record
+![alt text](<Screenshot 2024-07-17 185211.png>)
+
+After Creating fetch all records
+![alt text](<Screenshot 2024-07-17 185242.png>)
+
+Update Todo 1
+![alt text](<Screenshot 2024-07-17 191050.png>)
+All records
+![alt text](<Screenshot 2024-07-17 191142.png>)
+
+
+Delete Todo 1
+![alt text](<Screenshot 2024-07-17 191537.png>)
+
+Display all
+![alt text](<Screenshot 2024-07-17 191605.png>)
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // const express=require('express'); or we can import it
 import express from 'express';
 
-
+import bodyParser from 'body-parser';
 const app=express();
+app.use(bodyParser.json());
 app.all('/',(req,res)=>{
 //    console.log('Request received >',req);
 //    console.log('Response received >',res);
@@ -12,12 +13,12 @@ const todos=[
     {
         id:'1',
         title:'Task 1',
-        complted:false
+        completed:false
     },
     {
         id:'2',
         title:'Task 2',
-        complted:true
+        completed:true
     }
 ];
 //READ
@@ -37,8 +38,32 @@ app.post('/todos',(req,res)=>{
 })
 
 //UPDATE
-
+app.put('/todos/:id',(req,res)=>{
+   
+    const newTodoData=req.body;
+    const todoParamId=req.params.id;
+    const index=todos.findIndex(td=>td.id===todoParamId);
+   if(index!==-1){
+         todos[index]={
+            id:todoParamId,
+            ...newTodoData,
+         }
+        }
+    res.json({
+        message:'Todo has been updated'
+    });
+})
 //DELETE
+app.delete('/todos/:id',(req,res)=>{
+    const todoParamId=req.params.id;
+    const index=todos.findIndex(td=>td.id===todoParamId);
+    if(index!==-1){
+        todos.splice(index,1);
+    }
+    res.json({
+        message:'Todo has been deleted'
+    });
+}) 
 const PORT=5111;
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
